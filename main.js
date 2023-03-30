14ef89f06effd3920dd0c38d05235a4561fcefaf
tb1121 <Taylorball5@yahoo.com>
2023-03-29 21:07:32 -0700

    push all changes

---
+++ main.js
@@ -0,0 +1,23 @@
+//Attempted to change all instances of 's' and 'S' with '$'
+
+// HTMLElement.prototype.textNodes = function() {
+//     return [...this.childNodes].filter((node) => {
+//       return (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== "");
+//     });
+//   }
+
+//   document.querySelector('#testSubject').textNodes().forEach((node) => {
+//     node.textContent = 'S';});
+
+// const mono = "To be, or not to be--that is the question: Whether tis nobler in the mind to suffer. The slings and arrows of outrageous fortune Or to take arms against a sea of troubles";
+
+// const newMono = mono.split('');
+
+
+
+// const finalMono = newMono.forEach((element) => { if(element === "s" || element === "S") element = '$';}).toString().replace(",", "");
+
+// console.log(finalMono);
+
+
+
